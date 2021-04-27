bf2e610c19c62e49b3c891618ece3b366ceea78b
Quang Ly <quangdly@gmail.com>
2021-04-26 22:59:23 -0400

    domain files

---
+++ src/main/java/com/example/ec/domain/Difficulty.java
@@ -0,0 +1,14 @@
+package com.example.ec.domain;
+
+/**
+ * Created by quangly on 04/26/2020.
+ */
+
+/**
+ * Enumeration of the level of effort.
+ *
+ * Created by Quang Ly
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
